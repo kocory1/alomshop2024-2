a7c63069d9700232f1c7860756b9a35a32dc3ee9
subin <starbin7788@gmail.com>
2024-11-06 21:16:07 +0900

    initial commit

---
+++ alomshop/src/main/java/com/example/alomshop/AlomshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.alomshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlomshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlomshopApplication.class, args);
+    }
+
+}
---
+++ alomshop/src/test/java/com/example/alomshop/AlomshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alomshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlomshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
